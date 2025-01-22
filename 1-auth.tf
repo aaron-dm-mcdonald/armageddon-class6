8b1f687c2e7c9836461597bc810045f0b4ad211b
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-01-21 17:04:37 -0800

    update cloud code block, org, workspace for HCP Cloud

--- 1-auth.tf
+++ 1-auth.tf
@@ -6,12 +6,21 @@ terraform {
     }
   }
 
-  backend "s3" {
+ /*  backend "s3" {
     bucket  = "tf-backend-4357232"           # Name of the S3 bucket
     key     = "armageddon-modules-3.tfstate" # The name of the state file in the bucket
     region  = "us-east-1"                    # Use a variable for the region
     encrypt = true                           # Enable server-side encryption (optional but recommended)
-  }
+  } */
+
+  cloud { 
+    
+    organization = "test-4" 
+
+    workspaces { 
+      name = "armageddon-actions" 
+    } 
+  } 
 }
 
 provider "aws" {
